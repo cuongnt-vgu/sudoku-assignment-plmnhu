5b567df873a839cc66602133d1cf9e9974b4cf73
Nhu Pham Luu Minh <152388709+plmnhu@users.noreply.github.com>
2023-12-24 18:08:08 +0000

    abvccf

--- src/hidden_pairs.c
+++ src/hidden_pairs.c
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 #include "hidden_pairs.h"
 
 int hidden_pairs(SudokuBoard *p_board)
 {
+    aaaaaaaaaaaa
     return 0;
 }
-=======
-#include "hidden_pairs.h"
->>>>>>> 8b19c97 (added description)
