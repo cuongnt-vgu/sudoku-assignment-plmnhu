b8f9e93eefca94696c3faac7c26d7855f799f878
Nhu Pham Luu Minh <152388709+plmnhu@users.noreply.github.com>
2023-12-25 18:26:28 +0000

    avdf

--- src/hidden_triples.c
+++ src/hidden_triples.c
@@ -1,6 +1,159 @@
 #include "hidden_triples.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 int hidden_triples(SudokuBoard *p_board)
 {
     return 0;
+}
+
+
+void  XuLy_Mot_cell3(SudokuBoard *p_board,int row, int col, int *count)
+{
+   // printf(" ----- %d -----%d-----\n ", row, col);
+    if (p_board->data[row][col].num_candidates>1)
+    {
+        int n=-1,k;
+        for (int i=0 ; i<8; i++)
+            for (int j=i+1 ; j<9 ; j++)
+            if (( p_board->data[row][col].candidates[i]!=0) &&( p_board->data[row][col].candidates[j]!=0) )
+                {
+                    n=XuLy_cell_row(p_board,row,col,i,j);
+                    if (n>-1)
+                    {
+                        *count= *count+1;
+                        p_board->data[row][col].num_candidates=2;
+                        p_board->data[row][n].num_candidates=2;
+                        for(k=0;k<9;k++)
+                            if((k!=i) && (k!=j))
+                            {
+                                p_board->data[row][col].candidates[k]=0;
+                                p_board->data[row][n].candidates[k]=0;
+                            }
+
+                       // printf(" dong -- %d  %d  %d   %d   %d   \n", row,col,i,j,n);
+                    }
+                    n=XuLy_cell_col(p_board,row,col,i,j);
+                    if (n>-1)
+                    {
+                        *count = *count +1;
+                        p_board->data[row][col].num_candidates=2;
+                        p_board->data[n][col].num_candidates=2;
+                        for(k=0;k<9;k++)
+                            if((k!=i) && (k!=j))
+                            {
+                                p_board->data[row][col].candidates[k]=0;
+                                p_board->data[n][col].candidates[k]=0;
+
+                            }
+                        
+                       printf(" cot -- %d  %d  %d   %d   %d   \n", row,col,i,j,n);
+                       // for(k=0;k<9;k++)
+                         //   printf("  %d ",p_board->data[row][col].candidates[k]);
+                        printf("\n");
+                    }
+                        
+                    n=XuLy_cell_box(p_board,row,col,i,j);
+                    if (n>-1)
+                    {
+                        *count = *count+1;
+                        p_board->data[row][col].num_candidates=2;
+                        p_board->data[n/9][n%9].num_candidates=2;
+                        for(k=0;k<9;k++)
+                            if((k!=i) && (k!=j))
+                                {
+                                    p_board->data[row][col].candidates[k]=0;
+                                    p_board->data[n/9][n%9].candidates[k]=0;
+                                }
+                        //   printf(" box -- %d  %d  %d   %d   %d   \n", row,col,i,j,n);
+                    }
+                }
+    }
+}
+
+int  XuLy_cell_row3(SudokuBoard *p_board,int row,int col,int i,int j)
+{
+    int count=0;
+    int n_col=-1;
+    
+    for (int n=0 ; n<9; n++)
+        if (n!=col)
+        {
+            if ((p_board->data[row][n].num_candidates>2) ||(p_board->data[row][col].num_candidates>2))
+            {
+                if (( p_board->data[row][n].candidates[i]!=0) &&( p_board->data[row][n].candidates[j]!=0) )
+                    {
+                        count++;
+                        n_col = n;
+                        
+                        if (count==2)
+                            return -1;
+                    }
+                else 
+                    if (( p_board->data[row][n].candidates[i]!=0) || ( p_board->data[row][n].candidates[j]!=0) )
+                    {
+                        return -1;
+                    
+                    }
+            }
+        }
+
+    return n_col;
+}
+int  XuLy_cell_col3(SudokuBoard *p_board,int row,int col,int i,int j)
+{
+    int count=0;
+    int n_row=-1;
+    for (int n=0 ; n<9; n++)
+        if (n!=row)
+        {
+            if ((p_board->data[n][col].num_candidates>2) ||(p_board->data[row][col].num_candidates>2))
+            {
+                if (( p_board->data[n][col].candidates[i]!=0) &&( p_board->data[n][col].candidates[j]!=0) )
+                    {
+                        count++;
+                        n_row = n;
+                        if (count==2)
+                            return -1;
+                    }
+                else 
+                    if (( p_board->data[n][col].candidates[i]!=0) || ( p_board->data[n][col].candidates[j]!=0) )
+                    {
+                        return -1;
+                    }
+            }
+        }
+
+    return n_row;
+}
+int  XuLy_cell_box3(SudokuBoard *p_board,int row,int col,int i,int j)
+{
+    int row_box = (row/3)*3, col_box=(col/3)*3;
+    
+    
+    int count=0;
+    int n=-1;
+    for (int n_row=row_box ; n_row<row_box+3; n_row++)
+        for( int n_col = col_box ; n_col < col_box+3;n_col++)
+        if ((n_row!= row) || (n_col!=col))
+        { 
+            if ((p_board->data[n_row][n_col].num_candidates>2) ||(p_board->data[row][col].num_candidates>2))
+            {
+                if (( p_board->data[n_row][n_col].candidates[i]!=0) &&( p_board->data[n_row][n_col].candidates[j]!=0) )
+                    {
+                        count++;
+                        n = n_row*9 + n_col;
+                        if (count==2)
+                            return -1;
+                        
+                    }
+                else 
+                    if (( p_board->data[n_row][n_col].candidates[i]!=0) || ( p_board->data[n_row][n_col].candidates[j]!=0) )
+                    {
+                    
+                        return -1;
+                    }
+            }
+        }
+
+    return n;
 }
