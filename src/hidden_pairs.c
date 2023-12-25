8976e3a1dc26305ca118db98b841425ddbf20a02
Nhu Pham Luu Minh <152388709+plmnhu@users.noreply.github.com>
2023-12-25 05:14:00 +0000

    1234

--- src/hidden_pairs.c
+++ src/hidden_pairs.c
@@ -7,8 +7,8 @@ int hidden_pairs(SudokuBoard *p_board)
     int count = 0, countk;
     int foundk[10];
     int l=0;
-    int i,j,candidate;
-    int found[]={0;0;0;0;0;0;0;0;0};
+    int i,j,candidate,k;
+    int found[]={0,0,0,0,0,0,0,0,0};
     
     for ( i = 0; i < 9; i++) 
     {
@@ -16,12 +16,12 @@ int hidden_pairs(SudokuBoard *p_board)
         {
             if (p_board->data[i][j].num_candidates > 2) 
             {
-                for (int k = 0; k < 9; k++) 
+                for ( k = 0; k < 9; k++) 
                     {
                     found[k]=0;
                     foundk[k]=0;
                     }
-                for (int k = 0; k < 9; k++) 
+                for ( k = 0; k < 9; k++) 
                 {
                     candidate = p_board->data[i][j].candidates[k];
                     if (candidate !=0)
@@ -33,8 +33,10 @@ int hidden_pairs(SudokuBoard *p_board)
                         {
                             found[k]++;
                             if (found[k]>1)
-                         //   printf ("tim thay tren dong %d  %d %d  \n", k,  i, j);
-                            break;
+                            {
+                                printf ("tim thay tren dong %d  %d %d  \n", k,  i, j);
+                                break;
+                            }
                         }
                     if (found[k]>1)
                     {
@@ -45,12 +47,14 @@ int hidden_pairs(SudokuBoard *p_board)
                         {
                             found[k]++;
                             if (found[k]>1)
-                          //  printf (" tim thay tren cot %d  %d %d  \n", k , l,j);
+                            {    
+                                printf (" tim thay tren cot %d  %d %d  \n", k , l,j);
                                 break;
+                            }
                         }
                     if (found[k]>1)
                     {
-                    found = false;                            
+                    found[k] = 0;                            
                     int box = (i / 3) * 3 + j / 3;
                     int box_cell = (i % 3) * 3 + j % 3;
                     for (l = 0; l < 9; l++) 
@@ -62,15 +66,20 @@ int hidden_pairs(SudokuBoard *p_board)
                         {
                             found[k]++;
                             if(found[k]>1)
-                           // printf ("tim thay tren box   %d %d  %d \n", k, box , box_cell);
-                            break;
+                            {
+                                printf ("tim thay tren box   %d %d  %d \n", k, box , box_cell);
+                                break;
+                            }
                         }
                     }
                     }
-                    }
+            }
                     countk=0;
-                    for (int k = 0; k < 9; k++) 
-                        if(found[k]==1)
+                    for (l = 0; l < 9; l++) 
+                        printf("%d  ", found[k])
+                    printf("\n");
+                    for (l = 0; l < 9; l++) 
+                        if(found[l]==1)
                            { 
                             countk++;
                             foundk[countk]=k;
@@ -85,7 +94,7 @@ int hidden_pairs(SudokuBoard *p_board)
                         //   for ( m=0;m<9;m++)
                           //  p_board->data[i][j].candidates[m] = 0;
 
-                        for (int k = 0; k < 9; k++) 
+                        for ( k = 0; k < 9; k++) 
                         {
                             if ((k!=foundk[1]) && (k!=foundk[2]))
                                 p_board->data[i][j].candidates[k] = 0;
