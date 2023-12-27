049d385e98bac26c300e3bd51a6ccd2cf13a024c
Nhu Pham Luu Minh <152388709+plmnhu@users.noreply.github.com>
2023-12-27 18:29:45 +0000

    asd

--- src/naked_triples.c
+++ src/naked_triples.c
@@ -4,15 +4,15 @@
 #include <stdlib.h>
 int naked_triples(SudokuBoard *p_board)
 {
-  // p_board->data[0][4].candidates[7]=0;
+   p_board->data[2][0].candidates[4]=0;
   //  p_board->data[0][4].candidates[5]=1;
    // cas_naked_triples cas[100];
    int cas1=-1,cas2=-1,cas3=-1;
     int r1,c1,r2,c2,r3,c3;
     int count=0;
-    r1 = 0;  c1 = 3;  r2=0;  c2 = 6; r3=0 ; c3 =4;
+    //r1 = 0;  c1 = 3;  r2=0;  c2 = 5; r3=0 ; c3 =8;
     // r1 = 0;  c1 = 8; r2=1;  c2 = c1; r3=2 ; c3 =c1;
-   // r1 = 0;  c1 = 4; r2=2;  c2 = 3 ; r3=0 ; c3 =4;
+    r1 = 2;  c1 = 0; r2=0;  c2 = 1 ; r3=1 ; c3 =1;
     count= pair_3cell(p_board,r1,c1,r2,c2,r3,c3,&cas1, &cas2,&cas3 );
     printf(" %d  %d  %d  %d  %d  %d  %d  %d  %d\n", r1,c1,r2,c2,r3,c3, cas1, cas2,cas3);
    //int n_cas,n;
@@ -28,15 +28,15 @@ int pair_3cell(SudokuBoard *p_board,int row1, int col1,int row2, int col2,int ro
 {
     //int count=0;
  //   int n_cas=0;
-    int cas[]={-1,-1,-1,-1,-1};
+    int cas[10];
     int n_cas1 = p_board->data[row1][col1].num_candidates;
     int n_cas2 = p_board->data[row2][col2].num_candidates;
     int n_cas3 = p_board->data[row3][col3].num_candidates;
     
-    
+    printf("các %d  %d  %d \n",n_cas1,n_cas2,n_cas3);
 
- //   int rd1=(row1/3)*3 , cd1=(col1/3)*3, rd2=(row2/3)*3;
- //   int cd2=(col2/3)*3 , rd3=(row3/3)*3 , cd3=(col3/3)*3;
+    int rd1=(row1/3)*3 , cd1=(col1/3)*3, rd2=(row2/3)*3;
+   int cd2=(col2/3)*3 , rd3=(row3/3)*3 , cd3=(col3/3)*3;
     
     if((n_cas1==1) || (n_cas2==1) || (n_cas3==1))
         return 0;
@@ -51,16 +51,17 @@ int pair_3cell(SudokuBoard *p_board,int row1, int col1,int row2, int col2,int ro
             {
                 n_cas++;
                 cas[n_cas]=i;
+                 if (n_cas>3) 
+                    return 0;
             }
         }
-        if (n_cas!=3) 
-            return 0;
+        
     
     *cas1 = cas[1];
     *cas2 = cas[2];
     *cas3 = cas[3];
     //tren cung mot dong
-/* 
+ 
     if ((row1==row2) && (row2==row3))
     {
         printf("11111.  da vao day");
@@ -77,8 +78,7 @@ int pair_3cell(SudokuBoard *p_board,int row1, int col1,int row2, int col2,int ro
         return 1;
     }
 
-*/
- /*  
+
     //tren cung mot cot
     if ((col1==col2) && (col2==col3))
     {
@@ -94,12 +94,12 @@ int pair_3cell(SudokuBoard *p_board,int row1, int col1,int row2, int col2,int ro
         }
         return 1;
     }   
-*/
-/*  
-    ////tren cung mot khoi   
+
+ 
+    //tren cung mot khoi   
     if ((rd1==rd2) && (rd2==rd3)&&(cd1==cd2) && (cd2==cd3))
     {
-        for(j=rd1;j<rd1+3;j++)   //duyet qua cac cell
+        for(int j=rd1;j<rd1+3;j++)   //duyet qua cac cell
         for(int k=cd1;k<cd1+3;k++)
         {
             if ( ((j==row1) && (k==col1)) || ((j==row2) && (k==col2)) || ((j==row3) && (k==col3)) )
@@ -111,7 +111,7 @@ int pair_3cell(SudokuBoard *p_board,int row1, int col1,int row2, int col2,int ro
               
         }
     }   
-*/
+
     
     return 1;
 
